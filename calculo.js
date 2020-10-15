c8a694983bc822722ca7e2bace100defa3641008
sara991 <yamis704@gmail.com>
2020-10-15 16:29:08 -0600

    Creacion de un meotodo, formulario e index

---
+++ calculo.js
@@ -0,0 +1,7 @@
+exports.calcular = (palabra) =>{
+    let par;
+    if(palabra=true){
+        par="Bombon";
+    }
+    console.log(par ,palabra);
+};
--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const express = require ("express");
 const exphbs = require ("express-handlebars");
-// Importar body parser que nos permite acceder al cuerpo de la petición HTTP
-
+const bodyParser = require("body-parser");
 
 const app= express();
+
+const { calcular } = require("./calculo");
+
+
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res, next) => {
+  res.render("formulario");
+});
+
+
+ app.post("/palabra", (req, res, next) => {
+ const { numero, numero2} = req.body;
+
+ calcular(numero, numero2 );
+
+ res.send("Petición aceptada");
+});  
+
+//app.post("/prestamo", (req, res, next) => {
+
+  // const { palabra } = req.body;
+
+ // const cuotas = calcularMetodoFrances(monto, tasaInteres, periodo);
+
+ // res.render("resultado_prestamo", { cuotas });
+//});
+
+
 
 // Indicar a express utilizar handlebars como template engine
 
