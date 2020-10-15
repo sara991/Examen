4d4127451c00914cfa99408072af6c7b8e04b2dc
sara991 <yamis704@gmail.com>
2020-10-15 14:42:43 -0600

    Creacion servidor

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express = require ("express");
 const exphbs = require ("express-handlebars");
 // Importar body parser que nos permite acceder al cuerpo de la petición HTTP
-const bodyParser = require("body-parser");
+
 
 const app= express();
 
 // Indicar a express utilizar handlebars como template engine
-app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
- 
-app.set("view engine", "hbs");
 
-// Habilitar body parse para leer los datos del cuerpo de peticiones POST
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(5000, () => {
     console.log("Servidor ejecutándose en el puerto 5000");
